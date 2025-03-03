335390e940061ac2c362d7c2f1f43e7a4d90a045
Maxime Moisant <maxime@maximemoisant.fr>
2025-03-03 17:20:02 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "users" => "users#index", as: :users
+  resources :services, except: [:edit, :update] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show, :destroy]
+  get "dashboard" => "dashboards#show", as: :dashboard
   # Defines the root path route ("/")
   # root "posts#index"
 end
