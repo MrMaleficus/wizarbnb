2df14c1a640821b6f3b896b75f89ea9166a33459
Maxime Moisant <maxime@maximemoisant.fr>
2025-03-06 16:03:15 +0100

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,4 @@ class UsersController < ApplicationController
     @bookings = @user.bookings
   end
 
-  #  def average_rating
-  #    @user = User.find(params[:id])
-  #    @services = @user.services
-  #    @average_rating = @services.map(&:average_rating).sum / @services.size
-  #  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :services, dependent: :destroy
   has_many :bookings, dependent: :destroy
+
+
+
+  def average_rating
+    self.services.map(&:rating).sum / self.services.size
+  end
+
+  def star_rating
+    plain_star = "<i class='fa-solid fa-star' style='color:gold'></i>"
+    half_star = "<i class='fa-solid fa-star-half' style='color:gold'></i>"
+    rating = self.average_rating.floor
+    stars = plain_star * rating
+    stars += half_star if self.average_rating - rating > 0.5
+    stars
+  end
+
+
+
 end
