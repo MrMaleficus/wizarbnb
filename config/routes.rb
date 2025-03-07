13e53d050ad03ae5ee07986ac21f9df895ccf5fd
Maxime Moisant <maxime@maximemoisant.fr>
2025-03-07 10:46:01 +0100

    toggle dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+
+  resources :services, only: [:new, :create]
   resources :users, only: [:index, :show]
 
   get "profile" => "users#profile", as: :profile
